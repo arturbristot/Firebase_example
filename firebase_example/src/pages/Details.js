bec5d347563201c57e8b9d383ea31b18be4d86e3
Artur Bristot <arturbristot123@gmail.com>
2024-05-29 22:39:29 -0300

    update

--- firebase_example/src/pages/Details.js
+++ firebase_example/src/pages/Details.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>foi</Text>
     </View>
   );
 }
--- firebase_example/src/pages/Task.js
+++ firebase_example/src/pages/Task.js
@@ -1,10 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native"; 
+import { useNavigation } from '@react-navigation/native'; // Importe o hook
+
 
 
 export default function App() {
-  return (
+    const navigation = useNavigation(); // Instancie o hook
+
+    return (
     <View>
-      <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Button title="Go to Details" onPress={() => navigation.navigate('Details')}/>
     </View>
   );
 }
